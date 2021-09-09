bd7c6f800cb02364ae06fc73f57500319fe23c14
32 <53249541+32n@users.noreply.github.com>
2021-09-09 16:12:36 +0000

    Lesson 3

---
+++ lessons/Lesson3a.cpp
@@ -0,0 +1,12 @@
+// Lesson3a.cpp
+// Author: Aidan Din
+// Demonstrates the size of unsigned and normal integers are the same
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "unsigned int size: " << sizeof(uint) << " bytes"<< "\n";
+	cout << "int size: " << sizeof(int) << " bytes" << endl;
+	return 0;
+}
---
+++ lessons/Lesson3b.cpp
@@ -0,0 +1,17 @@
+// Lesson3b.cpp
+// Author: Aidan Din
+// Calculates the area and circumference of a circle given the radius
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float radius;
+	float pi = 3.1415;
+
+	cout << "Input a radius: " << "\n";
+	cin >> radius;
+	cout << "Area: " << pi * radius * radius << "\n";
+	cout << "Circumference: " << pi * radius * 2 << endl;
+	return 0;
+}
