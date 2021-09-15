56750ea137179b91f46a9c068db0ad26213a401b
32 <53249541+32n@users.noreply.github.com>
2021-09-15 16:12:45 +0000

    Lesson 5

--- lessons/Lesson5.cpp
+++ lessons/Lesson5.cpp
@@ -7,4 +7,17 @@ using namespace std;
 
 int main() {
 	bool a, b;
+	cout << "Input two booleans\n";
+	cin >> a >> b;
+	bool temp = a & b;
+	cout << "AND: " << temp << "\n";
+	temp = a | b;
+	cout << "OR: " << temp << "\n";
+	temp = a ^ b;
+	cout << "XOR: " << temp << "\n";
+	temp = ~a;
+	cout << "NOT a: " << temp << "\n";
+	temp = ~b;
+	cout << "NOT b: " << temp << endl;
+	return 0;
 }
