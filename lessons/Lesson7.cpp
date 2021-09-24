4a8d65874c97e26884c7462b258b6229e2cc5532
32 <53249541+32n@users.noreply.github.com>
2021-09-24 16:15:12 +0000

    Lesson 7 Part 2 + Ref Stuff

--- lessons/Lesson7.cpp
+++ lessons/Lesson7.cpp
@@ -6,20 +6,19 @@
 #include <math.h>
 using namespace std;
 
-double circumference;
-
 double volume(double r) {
 	return 4 * M_PI * r * r * r / 3;
 }
 double volume(double r, double h) {
 	return M_PI * r * r * h;
 }
-void circum(double r) {
+void circum(double r, double& circumference) {
 	circumference = M_PI * 2 * r;
 }
 int main() {
 	double r = 5, h = 10;
-	circum(r);
+	double circumference;
+	circum(r, circumference);
 	cout << "Circumference: " << circumference << "\n";
 	cout << "Sphere Volume: " <<  volume(r) << "\n";
 	cout << "Cylinder Volume: " << volume(r, h) << endl;
---
+++ lessons/Lesson7_2.cpp
@@ -0,0 +1,22 @@
+// Lesson7_2.cpp
+// Author: Aidan Din
+// Accepts a nonnegative integer from the user and displays true in main if the integer is a perfect number.
+
+#include <iostream>
+using namespace std;
+
+void isPerfect(int n, bool& perfect) {
+	int sum = 0;
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) sum += i;
+	}
+	if (sum == n) perfect = true;
+}
+int main() {
+	int n;
+	cout << "Input an integer:\n";
+	cin >> n;
+	bool perfect = false;
+	isPerfect(n, perfect);
+	cout << (perfect ? "true" : "false") << endl;
+}
