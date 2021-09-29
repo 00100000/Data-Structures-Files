f31504b480fbef7f8ce290eb40e873591cc2d368
32 <53249541+32n@users.noreply.github.com>
2021-09-29 16:22:03 +0000

    Template

---
+++ projects/PointerFunctions.cpp
@@ -0,0 +1,22 @@
+// PointerFunctions.cpp
+// Author: Aidan Din
+// Converts numbers and strings to different bases
+
+#include <iostream>
+using namespace std;
+
+// swaps two ints using pointers
+void swap(int* a, int* b) {
+
+}
+// counts the number of vowels and consonants in a string
+void letterCount(string s, int* numVowels, int* numConsonants) {
+
+}
+// displays each element and their memory locations in an array
+void arrayInfo(int* arrPtr, int length) {
+
+}
+int main() {
+	return 0;
+}
