0e2c0849419fa3b04266604d236912a3929c9e91
32n <00100000spacebar@gmail.com>
2021-10-18 22:41:48 -0400

    Lesson 10

--- lessons/Lesson10.cpp
+++ lessons/Lesson10.cpp
@@ -7,34 +7,59 @@
 using namespace std;
 
 class Shape {
-	bool hasCurve;
+	bool curve;
 
 	public:
-		Shape(bool c) {
-			hasCurve = c;
+		bool getCurve() {
+			return curve;
+		}
+		void setCurve(bool c) {
+			curve = c;
 		}
 };
 
-class Polygon : Shape {
+class Polygon : public Shape {
 	int sides;
 
 	public:
-		Polygon(int s) {
+		int getSides() {
+			return sides;
+		}
+		void setSides(int s) {
 			sides = s;
 		}
 };
 
-class Triangle : Polygon {
-	int angles[3];
+class Triangle : public Polygon {
+	int first, second, third;
 
 	public:
 		Triangle(int a, int b, int c) {
-			angles[0] = a;
-			angles[1] = b;
-			angles[3] = c;
+			setCurve(false);
+			setSides(3);
+			first = a;
+			second = b;
+			third = c;
+		}
+
+		int getFirst() { return first; }
+		int getSecond() { return second; }
+		int getThird() { return third; }
+		void setAngles(int a, int b, int c) {
+			first = a;
+			second = b;
+			third = c;
 		}
 };
 
 int main() {
+	Triangle t = Triangle(3, 4, 5);
+
+	cout << "This shape is curved: " << t.getCurve() << "\n";
+	cout << "Number of sides: " << t.getSides() << "\n";
+	cout << "Angle 1: " << t.getFirst() << "\n";
+	cout << "Angle 2: " << t.getSecond() << "\n";
+	cout << "Angle 3: " << t.getThird() << endl;
+
 	return 0;
 }
