1710a5410840cb5339b32bea68b884bc45f20b10
32 <53249541+32n@users.noreply.github.com>
2021-11-08 17:22:18 +0000

    Complex numbers Template

---
+++ projects/ComplexNumbers.cpp
@@ -0,0 +1,31 @@
+// ComplexNumbers.cpp
+// Author: Aidan Din
+// Implements a Complex class with a variety of functions
+
+class Complex {
+	Complex conjugate() {
+
+	}
+	double norm() {
+
+	}
+	double real() {
+
+	}
+	double imag() {
+
+	}
+	double dotProduct(Complex other) {
+
+	}
+	Complex scalarProduct(double value) {
+
+	}
+	double angleBetween(Complex other) {
+
+	}
+}
+
+int main() {
+	return 0;
+}
