19b34dfdfec1a83ac4ea6040d6b6a7b589cfa433
32 <00100000spacebar@gmail.com>
2021-11-08 23:28:55 -0500

    Complex (WIP)

--- projects/ComplexNumbers.cpp
+++ projects/ComplexNumbers.cpp
@@ -2,29 +2,41 @@
 // Author: Aidan Din
 // Implements a Complex class with a variety of functions
 
+#include <iostream>
+#include <math.h>
+
 class Complex {
-	Complex conjugate() {
+	private:
+		int a, b;
+	public:
+		Complex(int x, int y) {
+			a = x;
+			b = y;
+		}
+		Complex conjugate() {
+			Complex ret(a, -b);
+			return ret;
+		}
+		double norm() {
+			return sqrt(abs(a * a + b * b));
+		}
+		double real() {
+			return a;
+		}
+		double imag() {
+			return b;
+		}
+		double dotProduct(Complex other) {
+			return a * other.a + b * other.b;
+		}
+		Complex scalarProduct(double value) {
+			Complex ret(a * value, b * value);
+			return ret;
+		}
+		double angleBetween(Complex other) {
 
-	}
-	double norm() {
-
-	}
-	double real() {
-
-	}
-	double imag() {
-
-	}
-	double dotProduct(Complex other) {
-
-	}
-	Complex scalarProduct(double value) {
-
-	}
-	double angleBetween(Complex other) {
-
-	}
-}
+		}
+};
 
 int main() {
 	return 0;
