6c9afd68dcf02a5291eb642f2b131399576c3178
32 <53249541+32n@users.noreply.github.com>
2022-01-25 17:22:14 +0000

    Word Count WIP

---
+++ projects/WordCount.cpp
@@ -0,0 +1,16 @@
+// WordCount.cpp
+// Author: Aidan Din
+// Prints out the number of words in each file
+
+#include <fstream>
+#include <ifstream>
+#include <iostream>
+#include <ofstream>
+using namespace std;
+
+int main() {
+	string s;
+	cout << "Enter the name of a file:" << endl;
+	cin >> s;
+	return 0;
+}
