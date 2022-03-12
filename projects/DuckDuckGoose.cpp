7f99a6695ed6fc24ded8ea80fd381709edf4804d
00100000 <53249541+00100000@users.noreply.github.com>
2022-03-11 23:26:11 -0500

    DuckDuckGoose

---
+++ projects/DuckDuckGoose.cpp
@@ -0,0 +1,74 @@
+// Aidan Din
+// DuckDuckGoose.cpp
+// Simulates DuckDuckGoose with linked lists
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+class ListNode {
+	private:
+		int pos;
+		ListNode* next;
+		ListNode* prev;
+
+	public:
+		ListNode(int p, ListNode* n, ListNode* pr) {
+			pos = p;
+			next = n;
+			prev = pr;
+		}
+		// getters/setters
+		int getPos() {
+			return pos;
+		}
+		ListNode* getNext() {
+			return next;
+		}
+		ListNode* getPrev() {
+			return prev;
+		}
+		void setPos(int p) {
+			pos = p;
+		}
+		void setNext(ListNode* n) {
+			next = n;
+		}
+		void setPrev(ListNode* pr) {
+			prev = pr;
+		}
+};
+
+int main() {
+	string name;
+	cout << "Enter the name of the file you'd like to read from:\n";
+	cin >> name;
+	ifstream infile(name);
+	ofstream outfile("result.txt");
+
+	int cycle, participants;
+	infile >> participants >> cycle;
+	ListNode* head = new ListNode(1, nullptr, nullptr);
+	ListNode* last = head;
+	for (int i = 1; i < participants; i++) {
+		ListNode* temp = new ListNode(i + 1, nullptr, last);
+		last->setNext(temp);
+		last = temp;
+	}
+	last->setNext(head);
+	head->setPrev(last);
+
+	head = head->getPrev();
+	for (int i = 0; i < participants - 1; i++) {
+		for (int j = 0; j < cycle; j++) {
+			head = head->getNext();
+		}
+		outfile << "Trial " << i + 1 << ": " << head->getPos() << "\n";
+		// not clearing the memory, but whatever
+		head->getNext()->setPrev(head->getPrev());
+		head->getPrev()->setNext(head->getNext());
+	}
+
+	cout << flush;
+	return 0;
+}
